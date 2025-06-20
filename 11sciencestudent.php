34107fe9566fb5be6ab5cdbd428d51deb63119b6
Anubhav-8642 <subedianubhav941@gmail.com>
2025-06-20 17:28:23 +0545

    Add files via upload

---
+++ 11sciencestudent.php
@@ -0,0 +1,25 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$pass = "";
+$dbname = "student";
+$conn = new mysqli($servername ,$username ,$pass,$dbname);
+if($_SERVER['REQUEST_METHOD']=='POST'){
+$a = $_POST['rollno'];
+$b = $_POST['name'];
+$c = $_POST['address'];
+$d = $_POST['contact'];
+}
+if($conn){
+    
+}
+$abc="Insert into 11sciencestudent(Rollno,name,address,contact)values('$a','$b','$c','$d')";
+if($conn->query($abc)){
+   header('Location:student11science.html');
+}
+
+
+
+
+?>
---
+++ 12sciencestudent.php
@@ -0,0 +1,26 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$pass = "";
+$dbname = "student";
+$conn = new mysqli($servername ,$username ,$pass,$dbname);
+if($_SERVER['REQUEST_METHOD']=='POST'){
+$a = $_POST['rollno'];
+$b = $_POST['name'];
+$c = $_POST['address'];
+$d = $_POST['contact'];
+}
+if($conn){
+   
+}
+$abc="Insert into 12sciencestudent(Rollno,name,address,contact)values('$a','$b','$c','$d')";
+if($conn->query($abc)){
+    header('Location:student12science.html');
+    
+}
+
+
+
+
+?>
